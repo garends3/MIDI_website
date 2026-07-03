49b8f0476dd589900a01a8c88df57bdbbe7d7c7a
Arends-3 <g.c.arends-3@umcutrecht.nl>
2026-07-03 16:30:38 +0200

    remove taxlab

--- MIDI_Lab/wsgi.py
+++ MIDI_Lab/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'taxlab_website.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'MIDI_Lab.settings')
 
 application = get_wsgi_application()
