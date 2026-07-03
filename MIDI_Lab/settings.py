41e09eec62a61dbd4163da76ed46329c68d6edc0
Arends-3 <g.c.arends-3@umcutrecht.nl>
2026-07-03 16:26:34 +0200

    Key

--- MIDI_Lab/settings.py
+++ MIDI_Lab/settings.py
@@ -27,7 +27,8 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-%zu!w1u!i#$&co2wph@zu)3o^1$!p-douc7(xru*s(gc6sji49'
+SECRET_KEY = os.environ.get("SECRET_KEY")
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
