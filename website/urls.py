459af44044928f97ce9879fd472c69526e53f005
Arends-3 <g.c.arends-3@umcutrecht.nl>
2026-07-06 16:11:25 +0200

    Urls

--- website/urls.py
+++ website/urls.py
@@ -2,8 +2,21 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    # Main pages
     path("", views.home, name="home"),
     path("people/", views.people, name="people"),
     path("research/", views.research, name="research"),
     path("publications/", views.publications, name="publications"),
+
+    # People pages
+    path("people/chantal-tax/", views.chantal_tax, name="chantal_tax"),
+    path("people/christos-kanakis/", views.christos_kanakis, name="christos_kanakis"),
+    path("people/jamila-guichelaar/", views.jamila_guichelaar, name="jamila_guichelaar"),
+    path("people/leon-arends/", views.leon_arends, name="leon_arends"),
+    path("people/phebe-groenheide/", views.phebe_groenheide, name="phebe_groenheide"),
+
+    # News pages
+    path("news/ismrm-2026/", views.ismrm_2026, name="ismrm_2026"),
+    path("news/naturecom/", views.naturecom, name="naturecom"),
+    path("news/vidi-chantal/", views.vidi_chantal, name="vidi_chantal"),
 ]
